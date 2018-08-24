acccf5a9e863526650b23517b32665723a5b49d1
Zattelin <37785003+Zattelin@users.noreply.github.com>
2018-08-24 13:24:32 -0400

    commit for pulling from master

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Dashboard.css";
+
+const Dashboard = props => (
+  <div>
+    <ul>
+      <li className="homeList">
+      <img src="" alt=""/>
+      <div> 
+      <p>name</p>
+      <p>address</p>
+      <p>price</p>
+      <p>match count</p>
+      </div>
+     
+
+      </li>
+    </ul>
+  </div>
+);
+
+export default Dashboard;
